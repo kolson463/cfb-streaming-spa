d0ec9e8771915dbf43fa49741d9b330b7e643c59
kolson463 <98200634+kolson463@users.noreply.github.com>
2025-10-23 17:16:04 -0500

    added trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin(), tailwindcss()],
-  base: '/cfb-streaming-spa',
+  base: '/cfb-streaming-spa/',
   server: {
     port: 3000,
   },
